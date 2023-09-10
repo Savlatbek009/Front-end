278501a6f4a12d49b0cb8cfd5f079f8ed1a6d33d
Savlatbek Abdullayev <109302557+Savlatbek009@users.noreply.github.com>
2023-09-11 04:32:06 +0500

    Delete ReactJS/debt directory

--- ReactJS/debt/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function Footer() {
-  return (
-    <footer className="bg-custom text-white py-3">
-      <div className="text-center fs-5">insta: savlatbek_offic1al</div>
-    </footer>
-  );
-}
